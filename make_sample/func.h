3dab1b83652a969c43876661cda41f9603264b2e
ahyswang <qc_9223db36a73a4e57aa346d0af9b16558@git.code.tencent.com>
2019-03-22 10:42:52 +0800

    make 1

---
+++ make_sample/func.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+int add(int a, int b);
+
+#endif /* FUNC_H */
---
+++ make_sample/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "func.h"
+
+int main()
+{
+	printf("1 + 2 = %d\n", add(1, 2));
+	return 0;
+}
